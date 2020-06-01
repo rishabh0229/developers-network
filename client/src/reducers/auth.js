67cf96c92ce1de57257625c52e4e4e1c3a4d776a
Rishabh singh <rish29oo7@gmail.com>
2020-06-01 17:44:14 +0530

    final bug fixes

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -22,36 +22,39 @@ export default function(state=initialState,action){
 
 
 
-    switch(type){
-        case USER_LOADED:
-            return{
-                ...state,
-                isAuthenticated:true,
-                loading:false,
-                user:payload
-            }
-        case REGISTER_SUCCESS:
-        case LOGIN_SUCCESS:
-            localStorage.setItem("token", payload.token);
-            return{
-                ...state,
-                ...payload,
-                isAuthenticated:true,
-                loading:false
-            };
-        case REGISTER_FAIL:
-        case AUTH_ERROR:
-        case LOGIN_FAIL:
-        case LOGOUT:
-        case ACCOUNT_DELETED:
-            localStorage.getItem('token')
-            return{
-                ...state,
-                token:null,
-                isAuthenticated:false,
-                loading:false
-            }
-        default:
-            return state
+    switch (type) {
+      case USER_LOADED:
+        return {
+          ...state,
+          isAuthenticated: true,
+          loading: false,
+          user: payload,
+        };
+      case REGISTER_SUCCESS:
+      case LOGIN_SUCCESS:
+        localStorage.setItem("token", payload.token);
+        return {
+          ...state,
+          ...payload,
+          isAuthenticated: true,
+          loading: false,
+        };
+      case REGISTER_FAIL:
+      case AUTH_ERROR:
+      case LOGIN_FAIL:
+
+      case ACCOUNT_DELETED:
+        localStorage.getItem("token");
+        return {
+          ...state,
+          token: null,
+          isAuthenticated: false,
+          loading: false,
+        };
+      case LOGOUT:
+          localStorage.clear()
+          return state
+      default:
+        return state;
     }
 }
